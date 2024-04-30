b46fe7e0bde384b114c05b0dd70f6aa0603ac1e2
Favour Taiwo <princeisrael409@gmail.com>
2024-04-30 08:49:26 +0100

    metadata changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,18 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "fiittech.com",
+    template: "%s |",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "A web/mobile app developer",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "fiittech.com",
+    description: "A web/mobile app developer",
+    url: "https://portfolio-new-theta-amber.vercel.app/",
+    siteName: "fiittech.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://portfolio-new-theta-amber.vercel.app/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +37,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Fiittech",
     card: "summary_large_image",
   },
   icons: {
@@ -66,8 +65,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
